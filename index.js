1d852b53cfdc4b8982b241b8b35ad687e629bc08
lvmendess <liviamendes1703@gmail.com>
2025-07-27 22:45:35 -0300

    commit 3 - mais extract functions

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ function gerarFaturaStr (fatura, pecas) {
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
+    /*const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
+                            minimumFractionDigits: 2 }).format;*/
 
     for (let apre of fatura.apresentacoes) {
       let total = calcularTotalApresentacao(apre)
-  
-      // créditos para próximas contratações
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (getPeca(apre).tipo === "comedia") 
-         creditos += Math.floor(apre.audiencia / 5);
-  
-      // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      creditos += calcularCredito(apre)
+      
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
+  }
+
+  function formatarMoeda(valor){
+    return new Intl.NumberFormat("pt-BR",
+        { style: "currency", currency: "BRL",
+          minimumFractionDigits: 2 }).format(valor/100);
   }
 
   function calcularTotalApresentacao(apre){
@@ -50,7 +51,15 @@ function gerarFaturaStr (fatura, pecas) {
 
   function getPeca(apresentacao){
       return pecas[apresentacao.id]
-    }
+  }
+
+  function calcularCredito(apre){
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+      if (getPeca(apre).tipo === "comedia") 
+        creditos += Math.floor(apre.audiencia / 5);
+    return creditos;
+  }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
