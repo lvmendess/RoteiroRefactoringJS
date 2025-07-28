62c9996f02871d0da701528e6824d7569c3ca3d1
lvmendess <liviamendes1703@gmail.com>
2025-07-27 22:12:21 -0300

    commit 1 - extração de função

--- index.js
+++ index.js
@@ -10,7 +10,24 @@ function gerarFaturaStr (fatura, pecas) {
   
     for (let apre of fatura.apresentacoes) {
       const peca = pecas[apre.id];
-      let total = 0;
+      let total = calcularTotalApresentacao(apre, peca)
+  
+      // créditos para próximas contratações
+      creditos += Math.max(apre.audiencia - 30, 0);
+      if (peca.tipo === "comedia") 
+         creditos += Math.floor(apre.audiencia / 5);
+  
+      // mais uma linha da fatura
+      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      totalFatura += total;
+    }
+    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+    faturaStr += `Créditos acumulados: ${creditos} \n`;
+    return faturaStr;
+  }
+
+  function calcularTotalApresentacao(apre, peca){
+    let total = 0;
   
       switch (peca.tipo) {
       case "tragedia":
@@ -29,19 +46,7 @@ function gerarFaturaStr (fatura, pecas) {
       default:
           throw new Error(`Peça desconhecia: ${peca.tipo}`);
       }
-  
-      // créditos para próximas contratações
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
-         creditos += Math.floor(apre.audiencia / 5);
-  
-      // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
-      totalFatura += total;
-    }
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
-    return faturaStr;
+    return total;
   }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
